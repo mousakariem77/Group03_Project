f1ed9a6d508f06c5380d1caecc5ef7705d43f2c6
DinhAnh1243 <anhddce171053@fpt.edu.vn>
2024-03-06 23:11:56 +0700

    Payment

--- obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
+++ obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b17d9259e30d289fa31e6f6894e6be1cecf668a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444b6da865c059e0d30733fae81806955d1e3644")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
