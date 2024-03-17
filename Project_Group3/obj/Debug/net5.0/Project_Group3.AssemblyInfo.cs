f426d729dd08fe87c3497a6fb50e140b443ab967
Ka Riem <mousakariem27@gmail.com>
2024-03-17 23:21:13 +0700

    post

--- Project_Group3/obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
+++ Project_Group3/obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5134ec31c9074ee31cc4479bd29dd9570cc676")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd85b8c5bc2f711725713e33e8c8d5f70bc7514")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
